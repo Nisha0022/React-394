ffc2f1339d92a0e331ed4eed0ec195d7f6ffba76
Nisha <radhika@Nishas-MacBook-Air.local>
2025-02-04 11:59:29 +0530

    Server Task

--- myReactApp/src/App.jsx
+++ myReactApp/src/App.jsx
@@ -1,15 +1,30 @@
-
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+        const data = await response.json();
+        console.log(data);
+        setUsers(data);
+      } catch (e) {
+        console.error("Error fetching user data:", e);
+      }
+    };
+
+    fetchUserData();
+  }, []); 
 
   return (
-    <>
-      <div>
-        <h1>Hello World </h1>
-        </div>      
-    </>
-  )
+    <div>
+      <h1>Roll Number - 1/22/FET/BCS/394</h1>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
